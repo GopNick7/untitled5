4f7fa867ed8457c48df1f60927307a5b22a1ed8e
Nikita Romanenko <nikita.romanenko@chisw.com>
2019-03-19 14:41:15 +0200

    Initial commit

---
+++ lib/src/todo_list/todo_list_component.dart
@@ -0,0 +1,48 @@
+import 'dart:async';
+import "dart:math";
+
+import 'package:angular/angular.dart';
+import 'package:angular_components/angular_components.dart';
+
+import 'todo_list_service.dart';
+
+@Component(
+  selector: 'todo-list',
+  styleUrls: ['todo_list_component.css'],
+  templateUrl: 'todo_list_component.html',
+  directives: [
+    MaterialCheckboxComponent,
+    MaterialFabComponent,
+    MaterialIconComponent,
+    materialInputDirectives,
+    NgFor,
+    NgIf,
+  ],
+  providers: [ClassProvider(TodoListService)],
+)
+class TodoListComponent implements OnInit {
+  final TodoListService todoListService;
+
+  List<String> items = [];
+  String newTodo = '';
+  String randomElement = "";
+
+  TodoListComponent(this.todoListService);
+
+  @override
+  Future<Null> ngOnInit() async {
+    items = await todoListService.getTodoList();
+  }
+
+  void add() {
+    items.add(newTodo);
+    newTodo = '';
+  }
+
+  String remove(int index) => items.removeAt(index);
+
+  void onButtonClickListener(List<String> itemList) {
+    final random = new Random();
+    randomElement = itemList[random.nextInt(itemList.length)];
+  }
+}
